82c97c69128c783481edfd64936722ffb7057ede
francis omondi <francisomondi17@gmail.com>
2022-07-21 22:44:34 +0300

    added the morgan module to log request

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan")
 
 const app = express();
 
 dotenv.config({
     path: "config.env"
 });
-
 const PORT = process.env.PORT || 8080;
+
+//log request
+app.use(morgan("tiny"));
+
 
 app.get("/", (req, res) => {
     res.send("mawimbi seafood application");
