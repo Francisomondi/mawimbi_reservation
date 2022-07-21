bf6cf0cadedcc7ed517cdbdcd6b516083ceead4f
francis omondi <francisomondi17@gmail.com>
2022-07-21 23:01:15 +0300

    added body-parser, view engine and loaded assets file

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const morgan = require("morgan")
+const morgan = require("morgan");
+const bodyparser = require("body-parser");
+const path = require("path");
 
 const app = express();
 
@@ -11,6 +13,20 @@ const PORT = process.env.PORT || 8080;
 
 //log request
 app.use(morgan("tiny"));
+
+//parse request to body-parser
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
+
+//view engine
+app.set("view engine", "ejs");
+app.use("views", path.resolve(__dirname, "views/ejs"));
+
+//load assets
+app.use("/css", expree.static(path.resolve(__dirname, "assets/css")));
+app.use("/img", expree.static(path.resolve(__dirname, "assets/img")));
+app.use("/js", expree.static(path.resolve(__dirname, "assets/js")));
 
 
 app.get("/", (req, res) => {
