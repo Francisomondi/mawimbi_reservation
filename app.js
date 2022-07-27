c44521112277387391eb284a5afc2793383e9120
francis omondi <francisomondi17@gmail.com>
2022-07-27 19:49:20 +0300

    package files

--- app.js
+++ app.js
@@ -1,36 +1,16 @@
 const express = require("express");
-const dotenv = require("dotenv");
-const morgan = require("morgan");
+require("./models/db")
 const bodyparser = require("body-parser");
-const path = require("path");
+
 
 const app = express();
 
-dotenv.config({
-    path: "config.env"
-});
-const PORT = process.env.PORT || 8080;
 
-//log request
-app.use(morgan("tiny"));
 
 //parse request to body-parser
 app.use(bodyparser.urlencoded({
     extended: true
 }));
 
-//view engine
-app.set("view engine", "ejs");
-//app.use("views", path.resolve(__dirname, "views/ejs"));
 
-//load assets
-app.use("/css", express.static(path.resolve(__dirname, "assets/css")));
-app.use("/img", express.static(path.resolve(__dirname, "assets/img")));
-app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
-
-
-app.get("/", (req, res) => {
-    res.render("index")
-});
-
-app.listen(PORT, () => console.log(`server started runniing on port ${PORT}`));
+app.listen(3000, () => console.log(`server started runniing on port 3000`));
